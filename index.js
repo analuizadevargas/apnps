7b1b5d1439960059a3827a187b6840aa53430ecd
Ana <anavargas.bg110@academico.ifsul.edu.br>
2021-03-31 14:00:21 -0300

    Configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require ("express")
+const express = require ('express')
 const app = express()
+var bodyParser = require('express')
+var cookieParser = require('express')
+var path = require ('path') 
 
+app.use(cookieParser())
+
+app.use(bodyParser())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get ('/', function(req,res){
 res.send ("Olá Ana")
